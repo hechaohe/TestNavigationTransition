b67dd21224e9b358e180e9b061c0ca06cb5ea1cf
hechaohe <chao163hc@163.com>
2018-09-06 10:14:58 +0800

    Initial Commit

---
+++ TestNavigationTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestNavigationTransition
+//
+//  Created by 贺超 on 2018/9/6.
+//  Copyright © 2018年 贺超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestNavigationTransition/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestNavigationTransition
+//
+//  Created by 贺超 on 2018/9/6.
+//  Copyright © 2018年 贺超. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
